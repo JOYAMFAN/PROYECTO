f6fae3c546e290aaebc5705a25c3f89734a438ae
manuel <josemanuelgrandebautista@gmail.com>
2024-05-01 11:58:52 -0600

    avance tres

--- datos2.php
+++ datos2.php
@@ -1,6 +1,5 @@
 <?php
 include("conectar_bd2.php");
-$ID= $_POST['ID'];
 $CURP= $_POST['CURP'];
 $CORREO= $_POST['CORREO'];
 $NO_TELEFONO= $_POST['NO_TELEFONO'];
@@ -13,6 +12,29 @@ if(empty($CORREO))
 {
     ECHO "Ingresa tu correo";
 }
+elseif (empty($CURP)) {
+    echo "Ingresa tu curp";
+}elseif(empty($NO_TELEFONO))
+{
+  echo "Ingresa tu numero telefonico";
+}
+elseif(empty($FECHA_NACIMIENTO))
+{
+    echo "Ingresa tu fecha de nacimiento";
+}
+elseif(empty($DOMICILIO))
+{
+    echo"Ingresa tu domicilio";
+}
+elseif(empty($SEMESTRE))
+{
+    echo "Ingresa tu semestre";
+}
+elseif(empty($CARRERA))
+{
+    echo "Ingresa tu carrera";
+}
+else{
 $sql="INSERT INTO datos VALUES('$ID','$CURP','$CORREO','$NO_TELEFONO','$FECHA_NACIMIENTO','$DOMICILIO','$SEMESTRE','$CARRERA')";
 $query = mysqli_query($conexion,$sql);
 
@@ -22,5 +44,5 @@ if($query){
 
 
 }
-    
+}
 ?>
